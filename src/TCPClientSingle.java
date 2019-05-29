831be21abfbd748a46e2bee2aa5dbb00d910b50e
nobot <Gustav.bohlmark@gmail.com>
2019-05-30 00:09:06 +0200

    Cleanup

--- src/TCPClientSingle.java
+++ src/TCPClientSingle.java
@@ -14,7 +14,7 @@ public class TCPClientSingle {
     static String SERVER_ADDRESS = "169.254.10.219";
 
     public static void clientRun(int FILES_TO_RECIEVE) throws IOException {
-		BufferedWriter resultOut = new BufferedWriter(new FileWriter("TCPSingleTimes.txt"));
+
         File file = new File(FILE_TO_RECIEVE);
 		double FILESIZE_TO_RECIEVE = 0;
 		Duration onlyRecieve = Duration.ZERO;
@@ -24,38 +24,42 @@ public class TCPClientSingle {
         Duration timeForAllFiles;
         InetSocketAddress serverAddr = new InetSocketAddress(SERVER_ADDRESS,
                 SERVER_PORT);
-
-    	
-
-        for (int i = 0; i < FILES_TO_RECIEVE; i++) {
+		Duration elapsedTime  = Duration.ZERO;
+		Instant start = Instant.now();
+		Instant current = Instant.now();
+		FILES_TO_RECIEVE = 0;
+		while(Duration.between(start, current).getSeconds()<80){
+     //   for (int i = 0; i < FILES_TO_RECIEVE; i++) {
           	Instant inst = Instant.now();
             Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
             byte[] contents = new byte[10000];
 
-            BufferedOutputStream bufferedOutputStream =
-                    new BufferedOutputStream(new FileOutputStream(file));
             InputStream inputStream = socket.getInputStream();
 
             //No of bytes read in one read() call
             int bytesRead = 0;
-
+            BufferedOutputStream bufferedOutputStream =
+                    new BufferedOutputStream(new FileOutputStream(file));
+					
             while((bytesRead=inputStream.read(contents))!=-1)
                 bufferedOutputStream.write(contents, 0, bytesRead);
-
             bufferedOutputStream.flush();
+			
+            socket.close();
+			bufferedOutputStream.close();
 			Instant last = Instant.now();
 			getTime = Duration.between(inst,last);
-            socket.close();
 			onlyRecieve = onlyRecieve.plus(Duration.between(inst,last));		
             long test = getTime.toMillis();
-            resultOut.write(test + "\n");
 			System.out.println("Receive time: "+getTime.toMillis());
-			if(i == 0){
+			if(FILES_TO_RECIEVE == 0){
 			FILESIZE_TO_RECIEVE = file.length();
 			}
-			 file.delete();
+		FILES_TO_RECIEVE++;
+		 file.delete();
+		 current = Instant.now();
         }
-		resultOut.close();
+
  
         double totalFileSizeMB = (FILESIZE_TO_RECIEVE * FILES_TO_RECIEVE) / 1000000.0;
         double totalDurationSec = onlyRecieve.toNanos() / 1000000000.0;
